c70e0269ea2939b808f386b6fa208e041e8afcd1
Gregory Monroe II <gregthemusician@gmail.com>
2020-08-10 13:59:41 -0400

    removed unnecessary components within Login.jsx

--- client/src/containers/Login/Login.jsx
+++ client/src/containers/Login/Login.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
-// import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-// import FormControlLabel from '@material-ui/core/FormControlLabel';
-// import Checkbox from '@material-ui/core/Checkbox';
-// import Link from '@material-ui/core/Link';
-// import Grid from '@material-ui/core/Grid';
-// import Box from '@material-ui/core/Box';
-// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
@@ -33,10 +26,6 @@ import Container from '@material-ui/core/Container';
       flexDirection: 'column',
       alignItems: 'center',
     },
-    avatar: {
-      margin: theme.spacing(1),
-      backgroundColor: theme.palette.secondary.main,
-    },
     form: {
       width: '100%', // Fix IE 11 issue.
       marginTop: theme.spacing(1),
@@ -53,9 +42,6 @@ import Container from '@material-ui/core/Container';
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <div className={classes.paper}>
-            {/* <Avatar className={classes.avatar}>
-                <LockOutlinedIcon />
-            </Avatar> */}
             <Typography component="h1" variant="h5">
                 Login
             </Typography>
