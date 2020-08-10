02b378a651c64ff9ae3e18ebf14963eb9e0241ea
Gregory Monroe II <gregthemusician@gmail.com>
2020-08-10 14:35:05 -0400

    Login Page done. Need backend to connect submit button to viewworkout

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./containers/Home/Home";
 import NoMatch from "./containers/NoMatch/NoMatch";
 import Login from "./containers/Login/Login";
+import ViewWorkout from "./containers/ViewWorkout/ViewWorkout";
 
 
 function App() {
@@ -24,6 +25,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home}/>
           <Route exact path="/login" component={Login}/>
+          <Route exact path="/viewworkout" component={ViewWorkout}/>
           <Route component={NoMatch}/>
         </Switch>
       </Router>
--- client/src/containers/Login/Login.jsx
+++ client/src/containers/Login/Login.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
+// import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
@@ -68,20 +69,22 @@ import Container from '@material-ui/core/Container';
                     id="password"
                     autoComplete="current-password"
                 />
-                <Button
-                    type="submit"
-                    // fullWidth
-                    variant="contained"
-                    color="primary"
-                    className={classes.submit}
-                >
-                Login
-                </Button>
+                {/* <Link href="/viewworkout"> */}
+                    <Button
+                        type="submit"
+                        // fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                    Login
+                    </Button>
+                {/* </Link> */}
             </form>
         </div>
             {/* <Box mt={8}>
             <Copyright />
             </Box> */}
-        </Container>
+      </Container>
     );
   }
