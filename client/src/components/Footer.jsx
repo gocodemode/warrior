0621590454aa2411651253538487ad8893d0bc7f
Christina Starr <christinastarr19@gmail.com>
2020-08-11 10:48:06 -0400

    added react bootstrap npm package.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import
+import { Link } from 'react-router-dom';
+
+const styles = {
+    footer: { position: "fixed",
+        left: 0,
+        bottom: 0,
+        width: "100%",
+        backgroundColor: "gray",
+        color: "white",
+        },
+    alignRight: { textAlign: "right"},
+    alignLeft: { textAlign: "left"}
+}
 
 
 const Footer = () => {
     return (
         <div>
-            <footer className="footer">
-  <p>Copyright Warrior</p>
+            <footer style={styles.footer}>
+  <p style={styles.alignRight}>Copyright Warrior</p> <p style={styles.alignRight}>Instructor ?<Link to="/login"></Link></p>
 </footer>
         </div>
     );
