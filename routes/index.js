054337a486d70b9074bb386d1e6746cd09ff32af
kalebgarrison <kalebgarrison@gmail.com>
2020-08-11 10:12:58 -0400

    added seed workouts

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,17 @@
-// const router = require("express").Router();
-// const workoutRoutes = require("./workouts");
+const path = require("path");
+const router = require("express").Router();
+const apiRoutes = require("./api");
 
-// // Workout routes
-// router.use("/workouts", workoutRoutes);
+// API Routes
+router.use("/api", apiRoutes);
 
-// module.exports = router;
+// If no API routes are hit, send the React app
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+module.exports = router;
+
+
+
+
--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,45 +1,65 @@
-// const mongoose = require("mongoose");
-// const db = require("../models");
+const mongoose = require("mongoose");
+const db = require("../models/index");
+// const Workouts = require("../models/workouts");
+// const Instructor = require("../models/instructor");
 
-// // This file empties the Books collection and inserts the books below
+// This file empties the Books collection and inserts the books below
 
-// mongoose.connect(
-//   process.env.MONGODB_URI ||
-//   "mongodb://localhost/warrior"
-// );
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/warrior"
+);
 
-// const workoutSeed = [
-//   {
-//     title: "Bikram Yoga",
-//     instructor: "Brian Warner",
-//     synopsis:
-//       "Hot yoga in a studio.",
-//     date: new Date(Date.now())
-//   },
-//   {
-//     title: "Afternoon Yoga",
-//     instructor: "Greg Monroe II",
-//     synopsis:
-//       "Yoga in the park",
-//     date: new Date(Date.now())
-//   },
-//   {
-//     title: "Yoga in the morning",
-//     instructor: "Kaleb Garrison",
-//     synopsis:
-//       "Yoga at sunrise.",
-//     date: new Date(Date.now())
-//   },
-// ];
+const workoutSeed = [
+  {
+    name: "Bikram Yoga",
+    description:
+      "Hot yoga in a studio.",
+    location:"Columbus",
+    Sunday: false,
+    Monday: false,
+    Tuesday: false,
+    Wednesday: false,
+    Thursday: false,
+    Friday: false,
+    Saturday: true
+  },
+  {
+    name: "Afternoon Yoga",
+    description:
+      "Yoga in the park",
+      location:"Savannah",
+      Sunday: false,
+      Monday: false,
+      Tuesday: false,
+      Wednesday: false,
+      Thursday: true,
+      Friday: false,
+      Saturday: false
+  },
+  {
+    name: "Yoga in the morning",
+    description:
+      "Yoga at sunrise.",
+      location:"Atlanta",
+      Sunday: false,
+      Monday: false,
+      Tuesday: false,
+      Wednesday: true,
+      Thursday: false,
+      Friday: false,
+      Saturday: false
+  },
+];
 
-// db.Workouts
-//   .remove({})
-//   .then(() => db.Workout.collection.insertMany(workoutSeed))
-//   .then(data => {
-//     console.log(data.result.n + " records inserted!");
-//     process.exit(0);
-//   })
-//   .catch(err => {
-//     console.error(err);
-//     process.exit(1);
-//   });
+db.Workouts
+  .remove({})
+  .then(() => db.Workouts.collection.insertMany(workoutSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
