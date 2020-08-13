3d1b3703869cd72208cdf547ed9424264c014e92
kalebgarrison <kalebgarrison@gmail.com>
2020-08-13 12:03:38 -0400

    added title to card

--- client/src/components/WorkoutCard.jsx
+++ client/src/components/WorkoutCard.jsx
@@ -9,7 +9,7 @@ const WorkoutCard = () => {
     <Card style={{ width: "18rem" }} style={{margin: "5px"}}>
       <Card.Img variant="top" src="/Images/yogaonroof.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Yoga on the Roof</Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
