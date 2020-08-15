ac24bf81dc8c6d91be2d1cf33a993fd2ce470f16
Christina Starr <christinastarr19@gmail.com>
2020-08-14 22:46:12 -0400

    added the forward slash in front of updateworkout path.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, withRouter } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
